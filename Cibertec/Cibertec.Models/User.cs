4a894ccc0b28733f2a5113b2f5725f3bea0d595b
Julio Velarde <pxjuvela@cibertec.edu.pe>
2017-09-16 12:17:31 -0500

    Configuracion inicial de modelos.

---
+++ Cibertec/Cibertec.Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Cibertec.Models
+{
+    public class User
+    {        
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Roles { get; set; }
+    }
+}
