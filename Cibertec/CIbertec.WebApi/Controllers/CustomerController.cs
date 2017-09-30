ffcf6eba4f523f9b70b1ac65d35e5f9cc7c77dc5
Victor.Moreno <victor.moreno@outlook.com.pe>
2017-09-30 14:05:34 -0500

    3009 - Proyecto WebApi Inicial

---
+++ Cibertec/CIbertec.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Cibertec.UnitOfWork;
+
+namespace CIbertec.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Customer")]
+    public class CustomerController : Controller
+    {
+        private readonly IUnitOfWork _unit;
+        public CustomerController(IUnitOfWork unit)
+        {
+            _unit = unit;
+        }
+
+        public IActionResult GetList()
+        {
+            return Ok(_unit.Customers.GetList());
+        }
+    }
+}
