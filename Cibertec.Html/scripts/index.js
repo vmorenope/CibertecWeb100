fa3cd9de4a18d83dc6277f6ac42d55cf76423c9d
Victor.Moreno <Victor.Moreno@outlook.com.pe>
2017-11-03 12:58:43 -0500

    Javascript_completo

---
+++ Cibertec.Html/scripts/index.js
@@ -0,0 +1,9 @@
+﻿(function (cibertec) {
+    cibertec.Index = {
+        currentYear: function () {
+            var today = new Date();
+            return today.getFullYear();
+        }
+    };
+    document.getElementById("date").innerHTML = cibertec.Index.currentYear();
+})(window.cibertec = window.cibertec || {});
---
+++ Cibertec.Html/scripts/video.js
@@ -0,0 +1,19 @@
+﻿
+(function (cibertec) {
+    cibertec.video = {
+        videoElement: document.getElementById("video"),
+        play: function () {
+            if (this.videoElement.paused) {
+                this.videoElement.play();
+            }
+        },
+        pause: function () {
+            if (this.videoElement.played) {
+                this.videoElement.pause();
+            }
+        },
+        stop: function () {
+            this.videoElement.currentTime = 0;
+        }
+    };
+})(window.cibertec = window.cibertec || {});
